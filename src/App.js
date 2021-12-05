695109b13652eb1273b6c35321aa350b8eff0ec3
Lincoln Yan <yyjlincoln@gmail.com>
2021-12-05 11:45:58 +1100

    Update: Added helloworld

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forTest: ['ElementA', 'ElementB'],
+      ifTest: true
+    }
+  }
+
+  render() {
+    return (
+      <div className="flex flex-col mx-auto max-w-xl my-10 mx-10 rounded-md shadow-lg font-bold py-4 px-4 bg-gray-100">
+        <div className="text-center text-2xl">
+          Hello world!
+        </div>
+        <div className="text-center mt-5">
+          {
+            this.state.forTest.map((el, index) => {
+              return (
+                <div className="bg-green-100">For-test! {el}, {index}</div>
+              )
+            })
+          }
+        </div>
+
+        <div className="mt-4">
+          <div className="flex flex-row justify-center cursor-pointer" onClick={() => {
+            this.setState({
+              ifTest: !this.state.ifTest
+            })
+          }}>
+            <div className="bg-gray-200 rounded-md px-2 py-2">Toggle</div>
+          </div>
+          <div className="text-center mt-5">
+            {
+              this.state.ifTest ?
+                <div className="bg-green-100">If-test!</div> :
+                <div className="bg-red-100">If-test!</div>
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return new Main()
 }
 
 export default App;
